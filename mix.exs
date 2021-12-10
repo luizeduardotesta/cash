4d1c240d949d291ba9c8447fd50079ebd99b19f4
Luiz Testa <luizsonecazzz@hotmail.com>
2021-12-10 19:48:33 -0300

    add deps exmachinima and credo

--- mix.exs
+++ mix.exs
@@ -43,7 +43,9 @@ defmodule Cash.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:ex_machina, "~> 2.7.0"},
+      {:plug_cowboy, "~> 2.5"},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
     ]
   end
 
